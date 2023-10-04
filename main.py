7d6b50f3f5d01dcdf9d5b765ab0e98c9cebd99ab
IvanBosovch <138058545+IvanBosovch@users.noreply.github.com>
2023-10-04 16:50:28 +0300

    corrected: show all, phone, add

--- main.py
+++ main.py
@@ -24,34 +24,39 @@ def change_func(*user):
 
 @input_error
 def phone_func(*user):
-    return dict_phone[user]
+    if user[1] not in dict_phone:
+        raise KeyError
+    return f'Phone: {dict_phone.get(user[1])}'
 
 def hello_func(*user):
     return 'How can I help you?'
 
 @input_error
 def show_all_func(*user):
-    return f'All dict {dict_phone}'
+    string = ''
+    for k, v in dict_phone.items():
+        string += f'{k} : {v}\n'
+    return string
 
 
 def main(*user):
-    if user[0] == 'add':
+    if user[0].lower() == 'add':
         return add_func(*user)
-    elif user[0] == 'change':
+    elif user[0].lower() == 'change':
         return change_func(*user)
-    elif user[0] == 'phone':
+    elif user[0].lower() == 'phone':
         return phone_func(*user)
-    elif user[0] == 'hello':
+    elif user[0].lower() == 'hello':
         return hello_func(*user)
-    elif user[0] == 'show' and user[1] == 'all':
+    elif user[0].lower() == 'show' and user[1].lower() == 'all':
         return show_all_func(*user)
     else:
         return "There is no such request"
 
 
 while True:
-    input_user = input('>>>').lower()
-    if input_user == 'exit':
+    input_user = input('>>>')
+    if input_user.lower() == 'exit':
         print('Good bye')
         break
     print(main(*input_user.split()))
